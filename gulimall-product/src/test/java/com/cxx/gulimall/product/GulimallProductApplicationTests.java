57c825ae44826fef29b04e7136e6b5821984235a
cxx <cxx772@163.com>
2021-02-04 22:40:25 +0800

    项目初始结构创建

---
+++ gulimall-product/src/test/java/com/cxx/gulimall/product/GulimallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cxx.gulimall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
